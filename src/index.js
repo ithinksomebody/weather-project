dd41f282335492d341d9115a9392e026f4c3ecfc
ithinksomebody <116773495+ithinksomebody@users.noreply.github.com>
2023-01-08 17:03:51 -0500

    Added a time function in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
 let now = new Date();
 now.toString();
 
